c724e298e6011f0f0d7230a8ed4bb62fa24f65d3
sandeepkumar466 <125995083+sandeepkumar466@users.noreply.github.com>
2026-06-21 02:10:41 +0530

    Add files via upload

---
+++ nifty-pwa/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nifty-trend-pwa-v1';
+const ASSETS = [
+'./',
+'./index.html',
+'./manifest.webmanifest',
+'./assets/icon-192.png',
+'./assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+});
+
+self.addEventListener('activate', event => {
+event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.match(event.request).then(response => response || fetch(event.request))
+);
+});
+
